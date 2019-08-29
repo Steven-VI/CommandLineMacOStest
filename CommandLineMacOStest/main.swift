1caa00b1b601e285190b299c5e1999bef28fdc28
Steven Van Iseghem <steven.van.iseghem@gmail.com>
2019-08-29 10:50:09 +0200

    first commit

---
+++ CommandLineMacOStest/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  CommandLineMacOStest
+//
+//  Created by Home on 29/08/2019.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+let n1 = 2
+
+let n2 = 1
+
+print(n1 + n2)
+
